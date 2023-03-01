a8eae736c10b6132dd14bf7e91916277cc1e9476
Zac Miles <zgm202@exeter.ac.uk>
2023-03-01 11:25:35 +0000

    Added repository for reading, adding, and removing exhibits.

--- EntityApi/Entity API/Program.cs
+++ EntityApi/Entity API/Program.cs
@@ -1,11 +1,39 @@
 ﻿using EntityAPI.Models;
+using EntityAPI.Repositories;
+
+// TODO: Insert Data  DONE
+//       Read Data    DONE
+//       Delete Data  DONE
+//       Create API
+//       Tests
 
 Console.WriteLine("Loading Context...");
+var exhibitRepository = new ExhibitRepository();
 
-using (var context = new Context()) 
+
+// Adding 
+var newExhibit = new Exhibit()
 {
-    var exhibits = context.Exhibits?.ToList();
-    Console.WriteLine(exhibits);
-}
+    Id = 1,
+    Name = "Healthcare",
+    Description = "An exhibit showing our recent medical advances.",
+    Museum = new Museum(),
+    OpeningTime = new TimeOnly(9, 0, 0),
+    ClosingTime = new TimeOnly(17, 0, 0),
+};
 
+exhibitRepository.AddNew(newExhibit);
+// 
+
+// Removing
+var idToRemove = 1;
+exhibitRepository.RemoveById(idToRemove);
+// 
+
+// Reading
+var exhibits = exhibitRepository.GetAll();
+
+foreach (var exhibit in exhibits)
+    exhibit.LogProperties();
+// 
 Console.WriteLine("Program complete.");
---
+++ EntityApi/Entity API/Repositories/ExhibitRepository.cs
@@ -0,0 +1,64 @@
+﻿using EntityAPI.Models;
+using System.Linq;
+
+namespace EntityAPI.Repositories
+{
+    public class ExhibitRepository : IRepository<Exhibit>
+    {
+        public bool AddNew(Exhibit newExhibit)
+        {
+            //TODO: Validate object, return false if invalid. Add error handling.
+
+            using (var context = new Context()) 
+            {
+                if (context.Exhibits != null)
+                {
+                    context.Exhibits?.Add(newExhibit);
+                    context.SaveChanges();
+                    return true;
+                }
+                else 
+                    return false;
+            }
+        }
+
+        public List<Exhibit> GetAll()
+        {
+            using (var context = new Context())
+            {
+                return context.Exhibits != null ? context.Exhibits.ToList() : new List<Exhibit>();
+            }
+        }
+
+        public Exhibit GetById(int id)
+        {
+            using (var context = new Context())
+            {
+                return context.Exhibits?.SingleOrDefault(e => e.Id == id) ?? new Exhibit();
+            }
+        }
+
+        public bool RemoveById(int id)
+        {
+            // TODO: Add error handling to return false if not deleted.
+
+            using (var context = new Context()) 
+            {
+                if (context.Exhibits == null)
+                    return false;
+
+                var objectToRemove = context.Exhibits?.SingleOrDefault(e => e.Id == id);
+
+                if (objectToRemove != default)
+                {
+                    context.Exhibits?.Remove(objectToRemove);
+                    context.SaveChanges();
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
