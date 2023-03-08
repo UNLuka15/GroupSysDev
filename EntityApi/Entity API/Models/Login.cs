a546a6c8c1969e07ccf187e5b509fc49c3cde213
Zac Miles <zgm202@exeter.ac.uk>
2023-03-08 16:39:16 +0000

    Made password properties on Login model nullable.

--- EntityApi/Entity API/Models/Login.cs
+++ EntityApi/Entity API/Models/Login.cs
@@ -9,9 +9,9 @@ namespace EntityAPI.Models
     public class Login
     {
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; } // encrypted
-        public string PreviousPassword { get; set; } // encrypted
+        public string? Email { get; set; }
+        public string? Password { get; set; } // encrypted
+        public string? PreviousPassword { get; set; } // encrypted
         public DateTime PasswordChanged { get; set; }
         public DateTime? LastLoggedIn { get; set; }
     }
