458623ba0e8be688bf83988158af8d3102ef5fe5
Zac Miles <zgm202@exeter.ac.uk>
2023-03-15 12:40:03 +0000

    Converted Museum Controller to inherit from base controller

--- EntityApi/Entity API/Controllers/MuseumController.cs
+++ EntityApi/Entity API/Controllers/MuseumController.cs
@@ -1,69 +1,15 @@
-﻿using EntityAPI.Models;
+﻿using EntityAPI.Factories;
+using EntityAPI.Models;
 using EntityAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EntityAPI.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
-    public class MuseumController : ControllerBase
+    public class MuseumController : WriteBaseController<Museum, MuseumRequestModel>
     {
-        [HttpGet("All")]
-        public IActionResult GetMuseumList()
-        {
-            var museumList = new MuseumRepository().GetAll();
-            return museumList != null ? Ok(museumList) : NoContent();
-        }
+        public override IModelFactory<Museum, MuseumRequestModel> _factory => new MuseumFactory();
 
-        [HttpGet("Single")]
-        public IActionResult GetMuseum(string id)
-        {
-            int intId;
-
-            if (Int32.TryParse(id, out intId))
-            {
-                var museum = new MuseumRepository().GetById(intId);
-                return museum != null ? Ok(museum) : NotFound();
-            }
-
-            return BadRequest();
-        }
-
-        [HttpPost("Add")]
-        public IActionResult AddMuseum([FromBody] MuseumRequestModel museumRequest) 
-        {
-            var newMuseum = new Museum();
-            var museumRepository = new MuseumRepository();
-
-            newMuseum.Name = museumRequest.Name;
-            newMuseum.Description = museumRequest.Description;
-
-            var existingMuseums = museumRepository.GetAll();
-            
-            if (existingMuseums == null || existingMuseums.Any(m => m.Code == museumRequest.Code))
-                return BadRequest($"There is already a museum with the code '{museumRequest.Code}'.");
-
-            newMuseum.Code = museumRequest.Code;
-
-            museumRepository.AddNew(newMuseum);
-
-            return Ok($"'{newMuseum.Name}' museum created with Id '{newMuseum.Id}'.");
-        }
-
-        [HttpDelete("Remove")]
-        public IActionResult RemoveMuseum(string id)
-        {
-            int intId;
-
-            if (Int32.TryParse(id, out intId))
-            {
-                var success = new MuseumRepository().RemoveById(intId);
-
-                return success ? Ok($"Museum with id '{intId}' successfully removed.")
-                               : NotFound($"Museum with id '{intId} not found.'");
-            }
-
-            return BadRequest();
-        }
+        public override IRepository<Museum> _repository => new MuseumRepository();
     }
 }
---
+++ EntityApi/Entity API/Factories/MuseumFactory.cs
@@ -0,0 +1,26 @@
+﻿using EntityAPI.Models;
+using EntityAPI.Repositories;
+
+namespace EntityAPI.Factories
+{
+    public class MuseumFactory : IModelFactory<Museum, MuseumRequestModel>
+    {
+        public Museum Create(MuseumRequestModel museumRequest)
+        {
+            var newMuseum = new Museum();
+            var museumRepository = new MuseumRepository();
+
+            newMuseum.Name = museumRequest.Name;
+            newMuseum.Description = museumRequest.Description;
+
+            var existingMuseums = museumRepository.GetAll();
+
+            if (existingMuseums == null || existingMuseums.Any(m => m.Code == museumRequest.Code))
+                throw new Exception($"There is already a museum with the code '{museumRequest.Code}'.");
+
+            newMuseum.Code = museumRequest.Code;
+
+            return newMuseum;
+        }
+    }
+}
