a1771a40946e66dd2b56eaab296ebaee762a2cf1
Zac Miles <zgm202@exeter.ac.uk>
2023-03-24 08:40:33 +0000

    Added more unit tests

--- EntityApi/EntityAPITests/MuseumControllerTests.cs
+++ EntityApi/EntityAPITests/MuseumControllerTests.cs
@@ -2,6 +2,7 @@ using EntityAPI.Controllers;
 using EntityAPI.Factories;
 using EntityAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace EntityAPITests
 {
@@ -20,6 +21,54 @@ namespace EntityAPITests
             var result = actualMuseums.Value as List<Museum>;
             
             Assert.AreEqual(expectedMuseums, result);
+        }
+
+        [TestMethod]
+        public void GetCorrectMuseum()
+        {
+            var controller = GetController();
+            var expectedMuseum = new MockMuseumRepository().GetById(2);
+
+            var actualMuseum = controller.GetSingle("2") as ObjectResult;
+            var result = actualMuseum.Value as Museum;
+
+            Assert.AreEqual(expectedMuseum, result);
+        }
+
+        [TestMethod]
+        public void DeleteCorrectMuseum()
+        {
+            var controller = GetController();
+            var museumToRemove = 2;
+
+            var removedMuseumResult = controller.RemoveItem(museumToRemove.ToString()) as ObjectResult;
+
+            var remainingMuseums = new MockMuseumRepository().GetAll();
+
+            Assert.IsTrue(!remainingMuseums.Any(m => m.Id == museumToRemove));
+        }
+
+        [TestMethod]
+        public void SuccessfullyCreateMuseum() 
+        {
+            var controller = GetController();
+
+            MuseumRequestModel requestModel = new MuseumRequestModel()
+            {
+                Code = "MU01",
+                Name = "Test Museum",
+                Description = "A museum built specifically for testing."
+            };
+
+            var addedMuseumResult = controller.AddItem(requestModel) as ObjectResult;
+            var result = addedMuseumResult.Value.ToString();
+
+            var regex = new Regex("([0-9]{1,})");
+            var matchResult = regex.Match(result).ToString();
+
+            var remainingMuseums = new MockMuseumRepository().GetAll();
+
+            Assert.IsTrue(remainingMuseums.Any(m => m.Id == Int32.Parse(matchResult)));
         }
     }
 }
